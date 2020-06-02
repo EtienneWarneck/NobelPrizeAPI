80a916f66e2ba4d8b48ebca7b4ba52ad91dc3393
Etienne Warneck <etienne.warneck@gmail.com>
2020-06-01 22:42:50 -0400

    axios call for button

--- my-app/src/components/pages/selected.js
+++ my-app/src/components/pages/selected.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import AuthorCard from '../AuthorCard/AuthorCard.js';
-import Toolbar from '../Toolbar/Toolbar';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
