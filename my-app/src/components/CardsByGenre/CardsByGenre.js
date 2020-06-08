f2bce1ad82c6a61242659c13f3d84e848d83a873
Etienne Warneck <etienne.warneck@gmail.com>
2020-06-08 18:12:43 -0400

    value is controlled element

--- my-app/src/components/CardsByGenre/CardsByGenre.js
+++ my-app/src/components/CardsByGenre/CardsByGenre.js
@@ -7,83 +7,73 @@ export class CardsByGenre extends Component {
     constructor() {
         super()
         this.state = {
-            category: ""
+            value: "", // the value typed in input
+            category: "" // the category from the API
+
         }
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     };
 
     componentDidMount() {
-        axios.get('http://api.nobelprize.org/2.0/nobelPrizes?/nobelPrizeCategory=eco')
+        axios.get('http://api.nobelprize.org/2.0/nobelPrizes?' + "category=eco")
             .then(res => {
                 console.log("RES.DATA", res.data)
+                // console.log("CAT", res.data.nobelPrizes[0].category);
+
                 this.setState({
                     // allCards: res.data.nobelPrizes,
                     // name: res.data.nobelPrizes[0]['laureates'][0]['knownName']['en'],
                     // awardYear: res.data.nobelPrizes[0].awardYear,
-                    category: res.data.nobelPrizes[0].category.en,
+                    value: res.data.nobelPrizes?.category?.en,
                     // motivation: res.data.nobelPrizes[0]['laureates'][0]['motivation']['en']
                 })
             }).
             catch(err => console.log(err))
     };
 
-    handleChange(event) {
-        this.setState({ category: event.target.value });
+    handleChange(event) { //handleChange runs on every keystroke to update the React state of value
+        this.setState({ value: event.target.value });
+        console.log("YOU'RE TYPPING")
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        console.log("YOU'RE SUBMITTING")
+        alert('You submitted: ' + this.state.value);
+
         axios.get('http://api.nobelprize.org/2.0/nobelPrizes?/nobelPrizeCategory=?' + this.state.category)
             .then(res => {
-                console.log("RES.DATA", res.data)
-                if (res.data === undefined) {
-                    this.setState({
-                        // name: res.data.nobelPrizes[0]['laureates'][0]['knownName']['en'],
-                        // awardYear: res.data.nobelPrizes[0].awardYear,
-                        // category: res.data.nobelPrizes[0].category.en,
-                        // motivation: res.data.nobelPrizes[0]['laureates'][0]['motivation']['en']
-                    })
-                }
-                if (res.data !== undefined) {
-                    this.setState({ error: "Category not found" })
-                }
+                // console.log("RES.DATA SUBMIT", res.data)
+                // console.log("CAT", res.data.nobelPrizes[0].category.en)
+                // if (res.data === undefined) {
+                this.setState({
+                    // name: res.data.nobelPrizes[0]['laureates'][0]['knownName']['en'],
+                    // awardYear: res.data.nobelPrizes[0].awardYear,
+                    category: res.data.nobelPrizes?.category?.en,
+                    // motivation: res.data.nobelPrizes[0]['laureates'][0]['motivation']['en']
+                })
+                // }
+                // if (res.data !== undefined) {
+                //     this.setState({ error: "Category not found" })
+                // }
             }).
             catch(err => console.log(err))
     };
 
-
-
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Category:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
                 <Card>
-                    <Card.Text>
-                        {this.state.error}
-                    </Card.Text>
-                    <form onSubmit={this.handleSubmit}>
-                        <input
-                            type="text"
-                            value={this.state.category}
-                            name="category"
-                            placeholder="Enter Category"
-                            onChange={this.handleChange} />
-                        <input
-                            type="submit"
-                            value="Submit" />
-                    </form>
-                    <Card.Body>
-                        {/* <Card.Title> {this.state.name} </Card.Title>
-                        <Card.Text>
-                            Cases Confirmed: {this.state.awardYear}
-                        </Card.Text> */}
-                        <Card.Text>
-                            Category: {this.state.category}
-                        </Card.Text>
-                        {/* <Card.Text>
-                            Total Recovered: {this.state.motivation}
-                        </Card.Text> */}
-                    </Card.Body>
+                    <Card.Title> {this.state.value} </Card.Title>
                 </Card>
             </div>
         )
