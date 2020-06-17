09f43014eb22b384513bbb8de65fa9476a1c67ae
Etienne Warneck <etienne.warneck@gmail.com>
2020-06-17 18:01:39 -0400

    remove files used for testing

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -1,19 +1,9 @@
 import React from 'react';
-// import Buttons from '../Buttons/Buttons'
-// import Button from 'react-bootstrap/Button';
-import Buttons from '../Buttons/Buttons';
-// import CardsDisplay from '../CardsDisplay/CardsDisplay'
-import WinnerCard from '../WinnerCard/WinnerCard';
-
-// import Selected from "../pages/selected";
-
 
 function Home() {
     return (
         <div>
             <div className="spacer"></div>
-            <Buttons />
-            <WinnerCard/>
         </div>
     )
 }
